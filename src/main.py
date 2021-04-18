0f58472da61bf1c3c93b8be60b3601d79659e689
JoshiDhima <s0190681@ad.ua.ac.be>
2021-04-18 21:54:42 +0200

    comment test and main.py

--- src/main.py
+++ src/main.py
@@ -10,14 +10,18 @@ from OptimisationVisitor import OptimisationVisitor
 from LLVMVisitor import LLVMVisitor
 
 if __name__ == '__main__':
+    # Path to benchmarks
     file_path = "src/tests/benchmarks/CorrectCode/"
+    # If a file as given as argument, read it as the inputstream
     if len(sys.argv) > 1:
         f1 = open(f"{file_path + sys.argv[1]}.c")
         input_stream = InputStream(f1.read())
         f1.close()
+    # Otherwise wait for input
     else:
         input_stream = InputStream(sys.stdin.readline())
 
+    # Generate CST
     lexer = CLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = CParser(token_stream)
@@ -26,24 +30,31 @@ if __name__ == '__main__':
     lisp_tree_str = tree.toStringTree(recog=parser)
     print(lisp_tree_str)
 
+    # Generate AST
     listener = ASTListener()
     walker = ParseTreeWalker()
     walker.walk(listener, tree)
     AST = listener.curr_node
 
+    # Create DOT file of AST
     AST.to_dot("AST")
-    visitor = ASTVisitor()
-    visitor.visit(AST)
 
+    # Optimise AST
     visitor_opt = OptimisationVisitor()
     visitor_opt.visit(AST)
 
+    # Create DOT file of optimised AST
+    AST.to_dot("AST_OPT")
+
+    # Check AST for semantical errors
     visitor_err = SemanticalErrorVisitor()
     visitor_err.visit(AST)
 
+    # Generate LLVM code from AST
     visitor_llvm = LLVMVisitor()
     visitor_llvm.visit(AST)
 
+    # Print out the generated LLVM
     print("\n".join(visitor_llvm.LLVM))
     
     # AST.to_dot("AST")
